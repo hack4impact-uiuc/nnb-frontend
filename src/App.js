7710caf3c7876c4df6c7d1be6e9643781a3f1600
Jeff DeLonge <delonge2@illinois.edu>
2017-11-08 23:32:43 -0600

    Fix bug from merge

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
           {!showPOIForm && (
             <div>
               <div className="nnb-map-container">
-                <NNBMap {...this.state} />
+                <NNBMap {...this.state} setSelectedPOI={this.setSelectedPOI} />
               </div>
               <div className="info-panel-container">
                 <InfoPanel {...this.state} />
