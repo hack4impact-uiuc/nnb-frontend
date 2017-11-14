fcdbe5d2344e94b517af3f7d5400378bd74f07cf
Varun Munjeti <vrunjeti@gmail.com>
2017-11-14 12:31:17 -0800

    api request generalization

--- src/utils/apiWrapper.js
+++ src/utils/apiWrapper.js
@@ -1,8 +1,28 @@
 import request from 'superagent'
 import { config } from './apiConfig'
 
+const REQUEST_METHODS = {
+  get: 'get',
+  post: 'post',
+  put: 'put',
+  delete: 'delete'
+}
+
+function createRequest(method, endpoint, options) {
+  let req = request[method](`${config.apiUrl}/${endpoint}`)
+  if (options) {
+    if (method === REQUEST_METHODS.get) {
+      req = req.query(options)
+    } else if ([REQUEST_METHODS.post, REQUEST_METHODS.put].includes(method)) {
+      req = req.params(options)
+    }
+  }
+  return req.then(response => response.body)
+}
+
 function getData() {
-  return request.get(`${config.apiUrl}/posts/1`).then(response => response.body)
+  const endpoint = 'posts/1'
+  return createRequest(REQUEST_METHODS.get, endpoint)
 }
 
 export default {
