a61fd4cba03d4087b76364c72a5bcfaf2e66bac6
Varun Munjeti <vrunjeti@gmail.com>
2017-12-21 06:49:34 -0600

    navbar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,30 @@
 import React, { PureComponent } from 'react'
-import {
-  Navbar,
-  ToggleButtonGroup,
-  ToggleButton,
-  Button
-} from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { Menu } from 'react-feather'
 
 class NavBar extends PureComponent {
   render() {
     return (
-      <Navbar inverse>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <div className="sidebar-menu" onClick={this.props.toggleSidebar}>
-              =
+      <Navbar>
+        <div className="navbar-content">
+          <div
+            className="navbar-content__item navbar-content__icon"
+            onClick={this.props.toggleSidebar}
+          >
+            {/* Make sure size is aligned with `navbar-content__icon` height! */}
+            <Menu size={25} />
+          </div>
+          <div className="navbar-content__item navbar-content__title ">NNB</div>
+          {this.props.showEdit && (
+            <div className="navbar-content__item" onClick={this.props.onEdit}>
+              {this.props.isEditing ? 'Disable Editing' : 'Enable Editing'}
             </div>
-            <Link to="/">NNB</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <ToggleButtonGroup
-          type="checkbox"
-          value={this.props.isEditing ? [1] : []}
-          onChange={this.props.onEdit}
-        >
-          {this.props.showEdit && <ToggleButton value={1}>Edit</ToggleButton>}
+          )}
           <Link to="/login">
-            <Button>Login</Button>
+            <div className="navbar-content__item">Login</div>
           </Link>
-        </ToggleButtonGroup>
+        </div>
       </Navbar>
     )
   }
