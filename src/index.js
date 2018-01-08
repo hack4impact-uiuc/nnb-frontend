ed72da6a70e201f90bda32d8bce3f5acef759d04
CodeBrew28 <arshiam2@illinois.edu>
2018-01-07 23:31:24 -0800

    getting there

--- src/index.js
+++ src/index.js
@@ -28,3 +28,25 @@ ReactDOM.render(
   // </Router>
   document.getElementById('root')
 )
+
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap-theme.css'
+// import './styles/index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
+// import Login from './Login'
+// import registerServiceWorker from './registerServiceWorker'
+
+// import { HashRouter as Router, Route } from 'react-router-dom'
+
+// ReactDOM.render(
+//   <Router>
+//     <div>
+//       <Route exact path="/" component={App} />
+//       <Route path="/login" component={Login} />
+//     </div>
+//   </Router>,
+//   document.getElementById('root')
+// )
+// registerServiceWorker()
