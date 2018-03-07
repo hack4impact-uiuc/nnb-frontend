4241811d7cc3e62c3eec98981ad38dfd72aa9824
Varun Munjeti <vrunjeti@gmail.com>
2018-03-06 23:59:57 -0600

    react router setup

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
 import stuff from './stuff.reducer'
 
 const rootReducer = combineReducers({
-  stuff
+  stuff,
+  router: routerReducer
 })
 
 export default rootReducer
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,18 @@
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer from '../reducers/rootReducer'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { routerMiddleware } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
 import thunk from 'redux-thunk'
+import rootReducer from '../reducers/rootReducer'
+
+export const history = createHistory()
+const middleware = [thunk, routerMiddleware(history)]
+const composedMiddleware = compose(applyMiddleware(...middleware))
 
 export default function configureStore() {
   return createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
+    composedMiddleware
   )
 }
