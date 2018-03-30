ae99b34f94e44e73a54fe613742ce29f2852d01e
Alex <awu1337@gmail.com>
2018-03-29 23:25:36 -0500

    api adapter and wrapper changes

--- src/utils/apiAdapters.js
+++ src/utils/apiAdapters.js
@@ -1,10 +1,11 @@
 /**
- * convert to api
+ * Convert to api
  */
-function convertToApiMap(map) {
+
+function convertToApiGetPOI(params) {
   return {
-    image_url: map.imageUrl,
-    map_year: map.year
+    map_year: params.mapYear,
+    story_id: params.storyId
   }
 }
 
@@ -12,9 +13,9 @@ function convertToApiPOI(poi) {
   return {
     name: poi.name,
     map_year: poi.mapByYear,
-    date: `${poi.date.format('YYYY')} - ${poi.date.format(
+    date: `${poi.date.format('YYYY')}-${poi.date.format(
       'MM'
-    )} - ${poi.date.format('DD')}`,
+    )}-${poi.date.format('DD')}`,
     description: poi.description,
     x_coord: poi.coordinateX,
     y_coord: poi.coordinateY,
@@ -30,39 +31,29 @@ function convertToApiPOI(poi) {
   }
 }
 
-function convertToApiStory(storyName) {
+function convertToApiMap(map) {
   return {
-    story_name: storyName
-    // poi_ids: array of poi ids
+    image_url: map.imageUrl,
+    map_year: map.year
   }
 }
 
-function convertToApiStoriesMultiple(poi, selectedStories) {
-  // what is this for
+function convertToApiGetStories(poiId) {
   return {
-    input_story_name_id: selectedStories,
-    input_poi_id: poi.id
+    poi_id: poiId
   }
 }
 
-function convertToApiEditStoriesMultiple(poiId, selectedStories) {
-  // what is this for
+function convertToApiStory(story) {
   return {
-    poi_id: poiId,
-    stories: selectedStories
+    story_name: story.name,
+    poi_ids: story.poiIds
   }
 }
 
 /**
- * convert from api
+ * Convert from api
  */
-function convertFromApiMap(map) {
-  return {
-    imageUrl: map.image_url,
-    year: map.map_year,
-    id: map._id
-  }
-}
 
 function convertFromApiPOI(poi) {
   return {
@@ -85,6 +76,14 @@ function convertFromApiPOI(poi) {
   }
 }
 
+function convertFromApiMap(map) {
+  return {
+    id: map._id,
+    imageUrl: map.image_url,
+    year: map.map_year
+  }
+}
+
 function convertFromApiStory(story) {
   return {
     id: story._id,
@@ -93,12 +92,116 @@ function convertFromApiStory(story) {
 }
 
 export default {
-  convertToApiMap,
+  convertToApiGetPOI,
   convertToApiPOI,
+  convertToApiMap,
+  convertToApiGetStories,
   convertToApiStory,
-  convertToApiStoriesMultiple,
-  convertToApiEditStoriesMultiple,
-  convertFromApiMap,
   convertFromApiPOI,
+  convertFromApiMap,
   convertFromApiStory
 }
+
+// end new
+
+// function convertToApiMap(map) {
+//   return {
+//     image_url: map.imageUrl,
+//     map_year: map.year
+//   }
+// }
+
+// function convertToApiPOI(poi) {
+//   return {
+//     name: poi.name,
+//     map_year: poi.mapByYear,
+//     date: `${poi.date.format('YYYY')} - ${poi.date.format(
+//       'MM'
+//     )} - ${poi.date.format('DD')}`,
+//     description: poi.description,
+//     x_coord: poi.coordinateX,
+//     y_coord: poi.coordinateY,
+//     links: poi.links.filter(link => !!link.url).map(link => ({
+//       link_url: link.url,
+//       display_name: link.urlName
+//     })),
+//     media: poi.content.map(content => ({
+//       content_url: content.contentUrl,
+//       caption: content.caption
+//     })),
+//     story_ids: poi.storyIds // not sure on how to do this one
+//   }
+// }
+
+// function convertToApiStory(storyName) {
+//   return {
+//     story_name: storyName
+//     // poi_ids: array of poi ids
+//   }
+// }
+
+// function convertToApiStoriesMultiple(poi, selectedStories) {
+//   // what is this for
+//   return {
+//     input_story_name_id: selectedStories,
+//     input_poi_id: poi.id
+//   }
+// }
+
+// function convertToApiEditStoriesMultiple(poiId, selectedStories) {
+//   // what is this for
+//   return {
+//     poi_id: poiId,
+//     stories: selectedStories
+//   }
+// }
+
+// /**
+//  * convert from api
+//  */
+// function convertFromApiMap(map) {
+//   return {
+//     imageUrl: map.image_url,
+//     year: map.map_year,
+//     id: map._id
+//   }
+// }
+
+// function convertFromApiPOI(poi) {
+//   return {
+//     id: poi._id,
+//     name: poi.name,
+//     date: poi.date,
+//     description: poi.description,
+//     coordinateX: poi.x_coord,
+//     coordinateY: poi.y_coord,
+//     mapByYear: poi.map_year,
+//     links: poi.links.map(link => ({
+//       url: link.link_url,
+//       urlName: link.display_name
+//     })),
+//     content: poi.media.map(content => ({
+//       contentUrl: content.content_url,
+//       caption: content.caption
+//     })),
+//     stories: poi.stories.map(convertFromApiStory)
+//   }
+// }
+
+// function convertFromApiStory(story) {
+//   return {
+//     id: story._id,
+//     name: story.story_name
+//   }
+// }
+
+// export default {
+//   convertToApiMap,
+//   convertToApiPOI,
+//   convertToApiStory,
+//   convertToApiStoriesMultiple,
+//   convertToApiEditStoriesMultiple,
+//   convertFromApiMap,
+//   convertFromApiPOI,
+//   convertFromApiStory
+// }
--- src/utils/apiWrapper.js
+++ src/utils/apiWrapper.js
@@ -10,6 +10,10 @@ const REQUEST_METHODS = {
   DELETE: 'delete'
 }
 
+const POIS_URL = 'pois'
+const MAPS_URL = 'maps'
+const STORIES_URL = 'stories'
+
 cloudinary.config(cloudinaryConfig)
 
 function createRequest(method, endpoint, options) {
@@ -25,149 +29,122 @@ function createRequest(method, endpoint, options) {
 }
 
 /**
- * Map
+ * New POIs
  */
+
+function getPOIs(params) {
+  return createRequest(
+    REQUEST_METHODS.GET,
+    POIS_URL,
+    adapters.convertToApiGetPOI(params)
+  )
+    .then(res => res.result.pois)
+    .then(res => res.map(adapters.convertFromApiPOI))
+}
+
+function getPOI(poiId) {
+  return createRequest(REQUEST_METHODS.GET, `${POIS_URL}/${poiId}`)
+    .then(res => res.result.poi)
+    .then(res => adapters.convertFromApiPOI(res))
+}
+
+function postPOI(poi) {
+  return createRequest(
+    REQUEST_METHODS.POST,
+    POIS_URL,
+    adapters.convertToApiPOI(poi)
+  )
+    .then(res => res.result.poi)
+    .then(res => adapters.convertFromApiPOI(res))
+}
+
+function editPOI(poi, poiId) {
+  return createRequest(
+    REQUEST_METHODS.PUT,
+    `${POIS_URL}/${poiId}`,
+    adapters.convertToApiPOI(poi)
+  )
+    .then(res => res.result.poi)
+    .then(res => adapters.convertFromApiPOI(res))
+}
+
+function deletePOI(poiId) {
+  return createRequest(REQUEST_METHODS.DELETE, `${POIS_URL}/${poiId}`).then(
+    res => res.success
+  )
+}
+
+/**
+ * New Maps
+ */
+
 function getMaps() {
-  return createRequest(REQUEST_METHODS.GET, 'maps')
-    .then(res => res.data)
+  return createRequest(REQUEST_METHODS.GET, MAPS_URL)
+    .then(res => res.result.maps)
     .then(res => res.map(adapters.convertFromApiMap))
 }
 
 function postMap(map) {
   return createRequest(
     REQUEST_METHODS.POST,
-    'maps',
+    MAPS_URL,
     adapters.convertToApiMap(map)
-  ).then(res => res.data)
+  )
+    .then(res => res.result.map)
+    .then(res => adapters.convertFromApiMap(res))
 }
 
 function deleteMap(mapId) {
-  return createRequest(REQUEST_METHODS.DELETE, `maps/${mapId}`).then(
-    res => res.data
+  return createRequest(REQUEST_METHODS.DELETE, `${MAPS_URL}/${mapId}`).then(
+    res => res.success
   )
 }
 
 /**
- * POI
+ * New Stories
  */
-function getPOIs() {
-  return createRequest(REQUEST_METHODS.GET, 'pois').then(res =>
-    res.data.map(adapters.convertFromApiPOI)
-  )
-}
 
-function getPOI(id) {
-  return createRequest(REQUEST_METHODS.GET, `pois/${id}`)
-    .then(res => res.data)
-    .then(res => res.map(adapters.convertFromApiPOI)[0])
-}
-
-function getPOIsByYear(year) {
-  return createRequest(REQUEST_METHODS.GET, `maps/years/${year}`).then(res => {
-    const pois = res.data.pois.map(adapters.convertFromApiPOI)
-    const map = adapters.convertFromApiMap(res.data.map[0])
-    return { map, pois }
-  })
-}
-
-function getPOIsByStory(storyId) {
-  return createRequest(REQUEST_METHODS.GET, `stories/${storyId}`).then(res =>
-    res.pois.map(adapters.convertFromApiPOI)
-  )
-}
-
-function postPOIToStories(poi, selectedStories) {
+function getStories(poiId) {
   return createRequest(
-    REQUEST_METHODS.POST,
-    'stories/add/multiple',
-    adapters.convertToApiStoriesMultiple(poi, selectedStories)
+    REQUEST_METHODS.GET,
+    STORIES_URL,
+    adapters.convertToApiGetStories(poiId)
   )
-}
-
-function editPOIStories(poiId, selectedStories) {
-  return createRequest(
-    REQUEST_METHODS.POST,
-    'stories/edit/multiple',
-    adapters.convertToApiEditStoriesMultiple(poiId, selectedStories)
-  )
-}
-
-function postPOI(poi) {
-  return createRequest(
-    REQUEST_METHODS.POST,
-    'pois',
-    adapters.convertToApiPOI(poi)
-  ).then(res => res.data)
-}
-
-function editPOI(poiId, poi) {
-  return createRequest(
-    REQUEST_METHODS.PUT,
-    `pois/${poiId}`,
-    adapters.convertToApiPOI(poi)
-  )
-}
-
-function deletePOI(poiId) {
-  return createRequest(REQUEST_METHODS.DELETE, `pois/${poiId}`).then(
-    res => res.data
-  )
-}
-
-/**
- * Story
- */
-function getStories() {
-  return createRequest(REQUEST_METHODS.GET, 'stories')
-    .then(res => res.data)
+    .then(res => res.result.stories)
     .then(res => res.map(adapters.convertFromApiStory))
 }
 
-function getStoriesByPOI(poiId) {
-  return createRequest(REQUEST_METHODS.GET, `getstories/${poiId}`).then(
-    res => res.story_ids
-  )
-}
-
-// TODO: api should take in story id, not name
-function getStory(name) {
-  return createRequest(REQUEST_METHODS.GET, `stories/${name}`).then(
-    res => res.data
-  )
-}
-
-function postStory(storyName) {
+function postStory(story) {
   return createRequest(
     REQUEST_METHODS.POST,
-    'stories',
-    adapters.convertToApiStory(storyName)
-  ).then(res => res.data)
+    STORIES_URL,
+    adapters.convertToApiStory(story)
+  )
+    .then(res => res.result.story)
+    .then(res => adapters.convertFromApiStory(res))
 }
 
-function editStory(storyId, storyName) {
+function editStory(story, storyId) {
   return createRequest(
     REQUEST_METHODS.PUT,
-    `stories/${storyId}`,
-    adapters.convertToApiStory(storyName)
-  ).then(res => res.data)
+    `${STORIES_URL}/${storyId}`,
+    adapters.convertToApiStory(story)
+  )
+    .then(res => res.result.story)
+    .then(res => adapters.convertFromApiStory(res))
 }
 
 function deleteStory(storyId) {
-  return createRequest(REQUEST_METHODS.DELETE, `stories/${storyId}`).then(
-    res => res.data
-  )
-}
-
-/**
- * Auth
- */
-function postLogin(loginInfo) {
-  return createRequest(REQUEST_METHODS.POST, 'login', loginInfo)
+  return createRequest(
+    REQUEST_METHODS.DELETE,
+    `${STORIES_URL}/${storyId}`
+  ).then(res => res.success)
 }
 
 /**
  * Cloudinary
  */
+
 function uploadImage(imageDataURL) {
   return cloudinary.uploader
     .upload(imageDataURL)
@@ -176,24 +153,193 @@ function uploadImage(imageDataURL) {
 }
 
 export default {
-  getMaps,
-  postMap,
-  deleteMap,
   getPOIs,
   getPOI,
-  getPOIsByYear,
-  getPOIsByStory,
-  postPOIToStories,
-  editPOIStories,
   postPOI,
   editPOI,
   deletePOI,
+  getMaps,
+  postMap,
+  deleteMap,
   getStories,
-  getStoriesByPOI,
-  getStory,
   postStory,
-  postLogin,
   editStory,
   deleteStory,
   uploadImage
 }
+
+// end new
+
+// /**
+//  * Map
+//  */
+// function getMaps() {
+//   return createRequest(REQUEST_METHODS.GET, MAPS_URL)
+//     .then(res => res.result.maps)
+//     .then(res => res.map(adapters.convertFromApiMap))
+// }
+
+// function postMap(map) {
+//   return createRequest(
+//     REQUEST_METHODS.POST,
+//     MAPS_URL,
+//     adapters.convertToApiMap(map)
+//   ).then(res => res.data)
+// }
+
+// function deleteMap(mapId) {
+//   return createRequest(REQUEST_METHODS.DELETE, `${MAPS_URL}/${mapId}`).then(
+//     res => res.data
+//   )
+// }
+
+// /**
+//  * POI
+//  */
+// function getPOIs() {
+//   return createRequest(REQUEST_METHODS.GET, POIS_URL).then(res =>
+//     res.data.map(adapters.convertFromApiPOI)
+//   )
+// }
+
+// function getPOI(id) {
+//   return createRequest(REQUEST_METHODS.GET, `${POIS_URL}/${id}`)
+//     .then(res => res.data)
+//     .then(res => res.map(adapters.convertFromApiPOI)[0])
+// }
+
+// function getPOIsByYear(year) {
+//   return createRequest(REQUEST_METHODS.GET, `${MAPS_URL}/years/${year}`).then(res => {
+//     const pois = res.data.pois.map(adapters.convertFromApiPOI)
+//     const map = adapters.convertFromApiMap(res.data.map[0])
+//     return { map, pois }
+//   })
+// }
+
+// function getPOIsByStory(storyId) {
+//   return createRequest(REQUEST_METHODS.GET, `${STORIES_URL}/${storyId}`).then(res =>
+//     res.pois.map(adapters.convertFromApiPOI)
+//   )
+// }
+
+// function postPOIToStories(poi, selectedStories) {
+//   return createRequest(
+//     REQUEST_METHODS.POST,
+//     'stories/add/multiple',
+//     adapters.convertToApiStoriesMultiple(poi, selectedStories)
+//   )
+// }
+
+// function editPOIStories(poiId, selectedStories) {
+//   return createRequest(
+//     REQUEST_METHODS.POST,
+//     'stories/edit/multiple',
+//     adapters.convertToApiEditStoriesMultiple(poiId, selectedStories)
+//   )
+// }
+
+// function postPOI(poi) {
+//   return createRequest(
+//     REQUEST_METHODS.POST,
+//     POIS_URL,
+//     adapters.convertToApiPOI(poi)
+//   ).then(res => res.data)
+// }
+
+// function editPOI(poiId, poi) {
+//   return createRequest(
+//     REQUEST_METHODS.PUT,
+//     `${POIS_URL}/${poiId}`,
+//     adapters.convertToApiPOI(poi)
+//   )
+// }
+
+// function deletePOI(poiId) {
+//   return createRequest(REQUEST_METHODS.DELETE, `${POIS_URL}/${poiId}`).then(
+//     res => res.data
+//   )
+// }
+
+// /**
+//  * Story
+//  */
+// function getStories() {
+//   return createRequest(REQUEST_METHODS.GET, STORIES_URL)
+//     .then(res => res.data)
+//     .then(res => res.map(adapters.convertFromApiStory))
+// }
+
+// function getStoriesByPOI(poiId) {
+//   return createRequest(REQUEST_METHODS.GET, `getstories/${poiId}`).then(
+//     res => res.story_ids
+//   )
+// }
+
+// // TODO: api should take in story id, not name
+// function getStory(name) {
+//   return createRequest(REQUEST_METHODS.GET, `${STORIES_URL}/${name}`).then(
+//     res => res.data
+//   )
+// }
+
+// function postStory(storyName) {
+//   return createRequest(
+//     REQUEST_METHODS.POST,
+//     'stories',
+//     adapters.convertToApiStory(storyName)
+//   ).then(res => res.data)
+// }
+
+// function editStory(storyId, storyName) {
+//   return createRequest(
+//     REQUEST_METHODS.PUT,
+//     `stories/${storyId}`,
+//     adapters.convertToApiStory(storyName)
+//   ).then(res => res.data)
+// }
+
+// function deleteStory(storyId) {
+//   return createRequest(REQUEST_METHODS.DELETE, `${STORIES_URL}/${storyId}`).then(
+//     res => res.data
+//   )
+// }
+
+// /**
+//  * Auth
+//  */
+// function postLogin(loginInfo) {
+//   return createRequest(REQUEST_METHODS.POST, 'login', loginInfo)
+// }
+
+// /**
+//  * Cloudinary
+//  */
+// function uploadImage(imageDataURL) {
+//   return cloudinary.uploader
+//     .upload(imageDataURL)
+//     .then(res => res.secure_url)
+//     .catch(err => console.error(err))
+// }
+
+// export default {
+//   getMaps,
+//   postMap,
+//   deleteMap,
+//   getPOIs,
+//   getPOI,
+//   getPOIsByYear,
+//   getPOIsByStory,
+//   postPOIToStories,
+//   editPOIStories,
+//   postPOI,
+//   editPOI,
+//   deletePOI,
+//   getStories,
+//   getStoriesByPOI,
+//   getStory,
+//   postStory,
+//   postLogin,
+//   editStory,
+//   deleteStory,
+//   uploadImage
+// }
