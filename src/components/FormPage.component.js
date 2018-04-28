df0e48abc6c7175008e3c1224aa50165928d3af4
Alex <awu1337@gmail.com>
2018-04-28 03:00:28 -0500

    remove formpage container

--- src/components/FormPage.component.js
+++ src/components/FormPage.component.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
 import { POIForm, InfoPanel } from './'
-import { Redirect } from 'react-router'
 import './../styles/App.css'
 
 class FormPage extends Component {
   render() {
-    const { isLoggedIn } = this.props
-    return !isLoggedIn ? (
-      <Redirect to="/login" />
-    ) : (
+    return (
       <div className="nnb-app">
         <div className="poi-form-container">
           <POIForm />
